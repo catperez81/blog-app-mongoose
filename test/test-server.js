5ae88ae347f5cda22c7acdf1482068e2b736d9ce
catperez81 <catperez81@gmail.com>
2018-06-10 19:48:16 -0700

    test file

---
+++ test/test-server.js
@@ -0,0 +1,118 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+const expect = chai.expect;
+
+// This let's us make HTTP requests
+// in our tests.
+// see: https://github.com/chaijs/chai-http
+chai.use(chaiHttp);
+
+
+describe('BlogPosts', function() {
+
+  // Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the that promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
+
+  // although we only have one test module at the moment, we'll
+  // close our server at the end of these tests. Otherwise,
+  // if we add another test module that also has a `before` block
+  // that starts our server, it will cause an error because the
+  // server would still be running from the previous tests.
+  after(function() {
+    return closeServer();
+  });
+
+
+  it('should list items on GET', function() {
+    // for Mocha tests, when we're dealing with asynchronous operations,
+    // we must either return a Promise object or else call a `done` callback
+    // at the end of the test. The `chai.request(server).get...` call is asynchronous
+    // and returns a Promise, so we just return it.
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+
+        const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+
+   it('should add a blog post on POST', function() {
+    const newPost = {
+      title: 'Lorem ip some',
+      content: 'foo foo foo foo',
+      author: 'Emma Goldman'
+    };
+    const expectedKeys = ['id', 'publishDate'].concat(Object.keys(newPost));
+
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(newPost)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.all.keys(expectedKeys);
+        expect(res.body.title).to.equal(newPost.title);
+        expect(res.body.content).to.equal(newPost.content);
+        expect(res.body.author).to.equal(newPost.author)
+      });
+  });
+
+  it('should error if POST missing expected values', function() {
+    const badRequestData = {};
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(badRequestData)
+      .catch(function(res) {
+        expect(res).to.have.status(400);
+      });
+  });
+
+  it('should update blog posts on PUT', function() {
+
+    return chai.request(app)
+      // first have to get
+      .get('/blog-posts')
+      .then(function( res) {
+        const updatedPost = Object.assign(res.body[0], {
+          title: 'connect the dots',
+          content: 'la la la la la'
+        });
+        return chai.request(app)
+          .put(`/blog-posts/${res.body[0].id}`)
+          .send(updatedPost)
+          .then(function(res) {
+            expect(res).to.have.status(204);
+          });
+      });
+  });
+
+  it('should delete posts on DELETE', function() {
+    return chai.request(app)
+      // first have to get
+      .get('/blog-posts')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blog-posts/${res.body[0].id}`)
+          .then(function(res) {
+            expect(res).to.have.status(204);
+          });
+      });
+  });
+
+});
